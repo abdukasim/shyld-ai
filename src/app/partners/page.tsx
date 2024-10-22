9cd3f1b1872f1d989596d41a7c202d6fc39b17d8
abdukasim <abdukasimr@gmail.com>
2024-10-22 15:24:03 +0300

    added coming soon pages for pages without designs

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,10 @@
+export default function Partners() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center p-8 bg-[#121212] shadow-lg rounded-lg">
+        <h1 className="text-3xl font-semibold text-white mb-4">Partners</h1>
+        <p className="text-xl text-gray-300">Coming Soon</p>
+      </div>
+    </div>
+  );
+}
